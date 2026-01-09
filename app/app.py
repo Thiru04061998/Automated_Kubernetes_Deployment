a3fbb2949a494bb28f43f4ce4d205aef61d5ec40
Thirumoorthy k <thirukathirvel1@gmail.com>
2026-01-10 00:03:29 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World - Deployed using Terraform, Docker, Kubernetes, and CI/CD"
+    return "Deployment done using EKS"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
