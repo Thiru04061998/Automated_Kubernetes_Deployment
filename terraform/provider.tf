0cb217fdd177dd43730b57c1cdbe513f44be1892
Thirumoorthy k <thirukathirvel1@gmail.com>
2026-01-09 11:28:35 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,5 +10,5 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = var.aws_region
 }
